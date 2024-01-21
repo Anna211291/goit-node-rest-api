9bdadd0c995eabd05820502a7808f9ecbfce49c2
Anna211291 <Anjutka777@gmail.com>
2024-01-21 12:02:02 -0500

    Summary

---
+++ controllers/contactsControllers.js
@@ -0,0 +1,13 @@
+import contactsService from "../models/contacts/index.js";
+
+export const getAllContacts = (req, res) => {
+    
+};
+
+export const getOneContact = (req, res) => {};
+
+export const deleteContact = (req, res) => {};
+
+export const createContact = (req, res) => {};
+
+export const updateContact = (req, res) => {};
